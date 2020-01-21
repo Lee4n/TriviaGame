5c5896f25d989ee74fafdedaf670094f46293d22
Lee Lawler <lawler_lee@icloud.com>
2020-01-21 17:20:23 -0600

    added reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
 
-    var timer, selectedQuestion;
+    var timer, selectedQuestion, cloneQuestions;
+
+    var score = {
+        correct: 0,
+        incorrect: 0
+    };
 
     var questions = [{
             ask: "what is 2+2?",
@@ -22,7 +27,7 @@ $(document).ready(function () {
             choices: [4, 8, 6, 7],
             answer: 8
         }
-    ]
+    ];
 
     $("#reset").hide();
     $(".answer").hide();
@@ -30,39 +35,67 @@ $(document).ready(function () {
 
     // start the game
     $("#start").on("click", function () {
-        $("#start").hide();
-        choiceGenerator();
-        $("#timer").show();
+        cloneQuestions = questions.slice();
+        reset();
     });
 
-    function choiceGenerator() {
-        selectedQuestion = questions[Math.floor(Math.random() * questions.length)];
-        var choicesArray = []
-        timer = 30;
+    $("#reset").on("click", function () {
+        questions = cloneQuestions.slice();
+        console.log(questions, "bob");
+        reset();
+    });
 
-        for (let i = 0; i < selectedQuestion.choices.length; i++) {
-            var choices = selectedQuestion.choices;
-            var choice = '<button class="choice">' + choices[i].toString() + '</button>'
-            choicesArray.push(choice)
+    // reset game
+    function reset() {
+        $("#reset").hide();
+        $("#start").hide();
+        score.correct = 0;
+        score.incorrect = 0;
+        choiceGenerator();
+        $("#timer").show();
+    };
+
+
+
+    function choiceGenerator() {
+        console.log(questions, "im the new questions");
+        if (questions.length !== 0) {
+            selectedQuestion = questions[Math.floor(Math.random() * questions.length)];
+            var choicesArray = []
+            timer = 5;
+            $("#timeRemains").text(timer);
+
+
+            for (let i = 0; i < selectedQuestion.choices.length; i++) {
+                var choices = selectedQuestion.choices;
+                var choice = '<button class="choice">' + choices[i].toString() + '</button>'
+                choicesArray.push(choice)
+            }
+            $('#questionWrapper').text(selectedQuestion.ask);
+            $('#choicesWrapper').empty();
+            $('#choicesWrapper').append(choicesArray);
+            questions.splice(questions.indexOf(selectedQuestion), 1);
+            console.log(questions);
+            $("#choicesWrapper").show();
+            winLoseConditions();
+        } else {
+            $("#questionWrapper").text("There are no more questions left! Here are your results.");
+            $("#choicesWrapper").text("Correct: " + score.correct + " Incorrect: " + score.incorrect);
+            $("#choicesWrapper").show();
+            $("#reset").show();
         }
-        $('#questionWrapper').text(selectedQuestion.ask);
-        $('#choicesWrapper').empty();
-        $('#choicesWrapper').append(choicesArray);
-        questions.splice(questions.indexOf(selectedQuestion), 1);
-        console.log(questions);
-        $("#choicesWrapper").show();
-        startTimer();
 
     }
 
-    function showDelay() {
+    function nextQuestionDelay() {
+        $("#choicesWrapper").hide();
         var delay = setInterval(function () {
             choiceGenerator();
             clearInterval(delay);
         }, 2000)
     };
 
-    function startTimer() {
+    function winLoseConditions() {
 
         $(".choice").on('click', function (e) {
 
@@ -74,10 +107,12 @@ $(document).ready(function () {
 
             if (selectedQuestion.answer == e.target.innerText) {
                 $("#questionWrapper").text("You were correct! " + selectedQuestion.answer + " was the answer.")
-                showDelay();
+                score.correct++
+                nextQuestionDelay();
             } else {
                 $("#questionWrapper").text("You were wrong! The correct answer was " + selectedQuestion.answer);
-                showDelay();                
+                score.incorrect++
+                nextQuestionDelay();
             }
         })
 
@@ -86,7 +121,10 @@ $(document).ready(function () {
                 timer--;
                 $("#timeRemains").text(timer);
             } else if (timer == 0) {
-
+                clearInterval(mainTimer);
+                $("#questionWrapper").text("You were wrong! The correct answer was " + selectedQuestion.answer);
+                score.incorrect++
+                nextQuestionDelay();
             }
         }, 1000)
 
